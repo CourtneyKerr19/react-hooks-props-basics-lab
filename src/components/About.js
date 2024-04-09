e44e7bfcd2f190d607d1482ff444024270189bf4
Courtney Kerr <courtneykerr219@gmail.com>
2024-04-09 05:19:12 -0500

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
+// About.js
 import React from "react";
+import Links from "./Links"; // Import the Links component
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* Conditional rendering for bio */}
+      {props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {/* Pass down github and linkedin links as props to Links component */}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+// Links.js
+import React from "react";
+
+function Links(props) {
+  return (
+    <div>
+      <h3>Links</h3>
+      {/* Display GitHub and LinkedIn links */}
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a>
+    </div>
+  );
+}
+
+export default Links;
